4358ba10a691eb339f7c41a34710f70d55de1466
michaelshaleen <shaleenmichael@yahoo.com>
2021-02-19 08:21:11 -0600

    starting axios post

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
+
+const fetchGallery = ()
+
+
 
 function App() {
+  const [galleryList, setGalleryList] = useState([]);
+
+  axios({
+    method: 'GET',
+    url: '/gallery'
+  })
+  .then(response => {
+    console.log(response.data, "res data")
+    setGalleryList(response.data);
+    console.log(galleryList);
+  })
+  .catch(err => {
+    console.log("err", err)
+  })
+
     return (
       <div className="App">
         <header className="App-header">
