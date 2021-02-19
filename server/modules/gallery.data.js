f91dda1507fa48d69a1b9f18e3f2f111f4e76a20
michaelshaleen <shaleenmichael@yahoo.com>
2021-02-19 08:10:20 -0600

    set up done, pictures added to images folder

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,21 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, 
+      title: 'Mountain Goat',
+      pathFor: 'images/goat_small.jpg', 
+      description: 'Photo of a goat taken at Glacier National Park.', likes: 0
+     }
+
+      { id:2,
+       title: 'Reef Fish',
+       pathFor: 'images/reef_fish.jpg',
+       description:'Underwater photo of Parrotfish', likes: 0
+      }
+      
+      { id:3,
+      title:'Lone Bear',
+      pathFor: 'images/bear.jpeg',
+      description: 'Artwork of a Bear', likes:0
+      }
 ];
 
 module.exports = galleryItems;
